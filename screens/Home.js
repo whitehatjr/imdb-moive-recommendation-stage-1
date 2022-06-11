f64b8198b9c67575c8ee420eefda91d39f3bef6c
Turkialq <you@example.com>
2022-06-11 23:31:29 +0300

    added some comments

--- screens/Home.js
+++ screens/Home.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { Header, AirbnbRating, Icon } from "react-native-elements";
 import { RFValue } from "react-native-responsive-fontsize";
 import axios from "axios";
-
+// yo yo 
 export default class HomeScreen extends Component {
   constructor() {
     super();
